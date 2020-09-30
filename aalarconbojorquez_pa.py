bc6cce5da09876637e3bcc24692731b30b495540
aalarconbojorque <aa.alarcon10@gmail.com>
2020-09-30 11:44:26 -0700

    quick mod

--- aalarconbojorquez_pa.py
+++ aalarconbojorquez_pa.py
@@ -305,7 +305,7 @@ def UpdateCommand(commandLine):
 
               
                 file.close()
-                print("Again")
+     
 
 
 # ----------------------------------------------------------------------------
